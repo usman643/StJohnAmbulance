41ab962e2948c63f648e5dadfdbfcd4d621694fa
Ali Haidar <alihaidar679@gmail.com>
2023-04-13 20:11:26 +0500

    New Dashboard view added

---
+++ StJhonsAmbulance/StJhonsAmbulance/Development/Controllers/NewDashboardVC/NewDashboardVC.swift
@@ -0,0 +1,29 @@
+//
+//  NewDashboardVC.swift
+//  StJhonsAmbulance
+//
+//  Created by Ali on 13/04/2023.
+//
+
+import UIKit
+
+class NewDashboardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
